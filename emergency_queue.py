33d17debcc0ca1de85f3fedaf579aa3245c9c133
Jwallace04 <jwallace04@rio.edu>
2025-10-28 16:41:14 -0400

    Update emergency_queue.py

--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,56 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
-
+    def __init__(self, name, urgency):
+        self.name = name
+        self.urgency = urgency
 
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = []
 
+    def heapify_up(self, index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.data[index].urgency < self.data[parent_index].urgency:
+                self.data[index], self.data[parent_index] = self.data[parent_index], self.data[index]
+                index = parent_index
+            else:
+                break
 
+    def heapify_down(self, index):
+        smallest = index
+        left = 2 * index + 1
+        right = 2 * index + 2
 
+        if left < len(self.data) and self.data[left].urgency < self.data[smallest].urgency:
+            smallest = left
+        if right < len(self.data) and self.data[right].urgency < self.data[smallest].urgency:
+            smallest = right
 
-# Test your MinHeap class here including edge cases
+        if smallest != index:
+            self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+            self.heapify_down(smallest)
+
+    def insert(self, patient):
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def remove_min(self):
+        if not self.data:
+            return None
+        if len(self.data) == 1:
+            return self.data.pop()
+        min_patient = self.data[0]
+        self.data[0] = self.data.pop()
+        self.heapify_down(0)
+        return min_patient
+
+    def peek(self):
+        if not self.data:
+            return None
+        return self.data[0]
+
+    def print_heap(self):
+        print("Current Queue:")
+        for patient in self.data:
+            print(f"- {patient.name} ({patient.urgency})")
